0f51ae0ade804bb0c845807f3ec445ba6e7721eb
xcCodebreaker <xccodebreaker619@gmail.com>
2025-02-20 19:48:57 +0600

    Add, Delete and Mark/Toggle done features are added

--- internal/task/task.go
+++ internal/task/task.go
@@ -27,3 +27,39 @@ func (tm *TaskManager) getNextID() int {
 
 	return tm.tasks[len(tm.tasks)-1].ID + 1
 }
+
+// Necessary for undo/redo functions. Any changes made in the program, this func will be run everytime.
+func (tm *TaskManager) saveState() {
+	state := make([]Task, len(tm.tasks))
+	copy(state, tm.tasks)
+	tm.history = append(tm.history, state)
+	tm.redoHistory = nil
+}
+
+func (tm *TaskManager) AddTask(description string) {
+	tm.saveState()
+	tm.tasks = append(tm.tasks, Task{
+		ID:          tm.getNextID(),
+		Description: description,
+		Done:        false,
+	})
+}
+
+func (tm *TaskManager) DeleteTask(id int) {
+	tm.saveState()
+	for i, t := range tm.tasks {
+		if t.ID == id {
+			tm.tasks = append(tm.tasks[:i], tm.tasks[i+1:]...)
+		}
+	}
+}
+
+func (tm *TaskManager) ToggleTaskDone(id int) {
+	for i, t := range tm.tasks {
+		if t.ID == id {
+			tm.saveState()
+			tm.tasks[i].Done = !t.Done
+			return
+		}
+	}
+}
